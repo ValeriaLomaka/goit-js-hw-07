a70a7e56969564b7285d4a2ac8ca3d234ef99b27
VALERIA <valerialomaka04@gmail.com>
2023-04-25 12:19:16 +0300

    add task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const createPhotosMarkup = galleryItems
+  .map(({ original, preview ,description}) => {
+    return `<li class="gallery__item">
+               <a class="gallery__link" href="${original}">
+               <img
+               class="gallery__image"
+               src="${preview}"
+               data-source="${original}"
+               alt="${description}"
+               />
+              </a>
+              </li>`; }).join("");
+
+const photosMarkup = createPhotosMarkup;
+gallery.insertAdjacentHTML("beforeend", photosMarkup);
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">`);
+    instance.show();
+
+    gallery.addEventListener('keydown', e => {
+        if (e.code === "Escape" ||
+            e.code === "Space" ||
+            e.code === "Enter") {
+          instance.close();
+        }
+    })  
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const createPhotosMarkup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  }).join("");
+
+const photosMarkup = createPhotosMarkup;
+gallery.insertAdjacentHTML("beforeend", photosMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+captionsData: "alt",
+captionDelay: 250,
+captionPosition: "bottom",
+});
+
+
